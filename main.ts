74337c975e9fb03dd202d3a21153e3749572aee0
yoyotastic9 <76183586+yoyotastic9@users.noreply.github.com>
2021-08-21 09:42:36 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Robux code A+B")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("HOLD ")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("A")
+})
