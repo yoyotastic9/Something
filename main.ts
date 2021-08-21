5e47c950f3b644ddd81198bea562f909bcb2b62b
yoyotastic9 <76183586+yoyotastic9@users.noreply.github.com>
2021-08-21 09:47:58 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Robux code A+B")
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("HOLD ")
+    basic.showString("Abx-yoa-pqow-73b")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("A")
